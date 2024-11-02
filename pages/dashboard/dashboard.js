fa5bdcb362b62881f3544eb04725463fcb9710dd
oknik <1254583588@qq.com>
2024-11-02 22:58:21 +0800

    login page

---
+++ pages/dashboard/dashboard.js
@@ -0,0 +1,10 @@
+Page({
+    data: {
+      // 可以添加您需要的数据
+    },
+  
+    onLoad: function(options) {
+      // 页面加载时的逻辑
+    }
+  });
+  
---
+++ pages/index/index.js
@@ -0,0 +1,46 @@
+// index.js
+const backgroundUrl = 'https://pic.imgdb.cn/item/6725bebdd29ded1a8c600e6d.jpg'
+const nameUrl = 'https://pic.imgdb.cn/item/6721e774d29ded1a8c1f12cf.png'
+const seeUrl = 'https://pic.imgdb.cn/item/67262438d29ded1a8cafcac2.png'
+const unseeUrl = 'https://pic.imgdb.cn/item/6726243cd29ded1a8cafcdb2.png'
+
+Page({
+  data: {
+    backgroundUrl: backgroundUrl,
+    nameUrl: nameUrl,
+    activeTab: 'password',
+    userPhone: '',
+    userPassword: '',
+    seeUrl: seeUrl,
+    unseeUrl: unseeUrl,
+    passwordVisible: true,
+    userCode: '',
+  },
+
+  switchTab(event) {
+    const { name } = event.currentTarget.dataset;
+    this.setData({
+      activeTab: name
+    });
+  },
+
+  togglePasswordVisibility() {
+    this.setData({
+      passwordVisible: !this.data.passwordVisible
+    });
+  },
+
+  sendCode() {
+
+  },
+
+  onLogin(){
+    wx.navigateTo({
+        url: '/pages/dashboard/dashboard'
+      });
+  },
+
+  onRegister(){
+
+  }
+})
