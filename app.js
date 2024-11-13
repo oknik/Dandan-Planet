494177075cfcf9491cb0d6588cf3a3c3b2ee953b
oknik <1254583588@qq.com>
2024-11-13 21:00:33 +0800

    首页

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
 // app.js
-App({})
+const avatarUrlDefault = 'https://pic.imgdb.cn/item/6728b79dd29ded1a8cd25236.jpg';
+
+class User {
+    constructor(name, level, stars, avatarUrl) {
+      this.userName = name;
+      this.userLevel = level;
+      this.userStars = stars;
+      this.userAvatar = avatarUrl;
+    }
+
+    // 更新等级
+    updateLevel(newLevel) {
+        this.userLevel = newLevel;
+    }
+    
+    // 增加星星数
+    addStars(stars) {
+        this.userStars += stars;
+    }
+
+    // 获取用户信息
+    getUserInfo() {
+        return {
+          userName: this.userName,
+          userLevel: this.userLevel,
+          userStars: this.userStars,
+          userAvatar: this.userAvatar
+        };
+    }
+}
+
+App({
+    globalData: {
+      userInfo: new User("kk", 3, 257, avatarUrlDefault)
+    }
+});
--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -1,10 +1,43 @@
+const app = getApp();
+const nameUrl = 'https://pic.imgdb.cn/item/673488b3d29ded1a8c89b493.png'
+const features = [
+  {
+    name: "语文",
+    imageUrl: "https://pic.imgdb.cn/item/67349ebcd29ded1a8c9b8610.jpg"
+  },
+  {
+    name: "数学",
+    imageUrl: "https://pic.imgdb.cn/item/67349071d29ded1a8c8f8926.jpg"
+  },
+  {
+    name: "英语",
+    imageUrl: "https://pic.imgdb.cn/item/67349a3cd29ded1a8c9805dd.jpg"
+  },
+  {
+    name: "绘本",
+    imageUrl: "https://pic.imgdb.cn/item/6734a0ccd29ded1a8c9d2211.jpg"
+  },
+  {
+    name: "？",
+    imageUrl: "https://example.com/images/english_letters.png"
+  },
+  {
+    name: "奖励",
+    imageUrl: "https://pic.imgdb.cn/item/6734a15ed29ded1a8c9d9b90.jpg"
+  }
+];
+
 Page({
-    data: {
-      // 可以添加您需要的数据
-    },
+  data: {
+    nameUrl,
+    features,
+    userInfo: {},
+  },
   
-    onLoad: function(options) {
-      // 页面加载时的逻辑
-    }
-  });
-  
+  onLoad() {
+    const userInfo = app.globalData.userInfo.getUserInfo();
+    this.setData({
+      userInfo: userInfo
+    });
+  }
+});
