9dd870fed47dce757f144f40730d2ad69cda2e9b
oknik <1254583588@qq.com>
2024-11-16 22:27:25 +0800

    数字学习-0 画布橡皮擦还有问题

--- pages/math/number/number.js
+++ pages/math/number/number.js
@@ -42,20 +42,6 @@ const imageData = [
     imageUrl: "https://pic.imgdb.cn/item/6738407dd29ded1a8c700f7d.png"
   }
 ];
-/*
-const imageData = [
-  { number: "zero", url: 'https://pic.imgdb.cn/item/67384037d29ded1a8c6fd15e.png' },
-  { number: "one", url: 'https://pic.imgdb.cn/item/6736ef7fd29ded1a8c5e0874.png' },
-  { number: "two", url: 'https://pic.imgdb.cn/item/6736efc7d29ded1a8c5e3d05.png' },
-  { number: 'three', url: 'https://pic.imgdb.cn/item/6736f1dbd29ded1a8c5fee27.png' },
-  { number: 'four', url: 'https://pic.imgdb.cn/item/6736f106d29ded1a8c5f09a8.png' },
-  { number: 'five', url: 'https://pic.imgdb.cn/item/6736f19cd29ded1a8c5fb35b.png' },
-  { number: 'six', url: 'https://pic.imgdb.cn/item/6738404fd29ded1a8c6fe931.png' },
-  { number: 'seven', url: 'https://pic.imgdb.cn/item/67384063d29ded1a8c6ffa46.png' },
-  { number: 'eight', url: 'https://pic.imgdb.cn/item/67384072d29ded1a8c7005c2.png' },
-  { number: 'nine', url: 'https://pic.imgdb.cn/item/6738407dd29ded1a8c700f7d.png' }
-]
-*/
 
 Page ({
     data: {
@@ -68,7 +54,7 @@ Page ({
         const path = e.currentTarget.dataset.path;
         console.log(path)
         wx.navigateTo({
-          url: `/pages/math/zero/zero`  // 根据路径跳转
+          url: `/pages/math/${path}`
         });
       }
 }) 
