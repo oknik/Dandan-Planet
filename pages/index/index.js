3ce3cb69aec5376ea652c91b41a4cf0cc14e9193
oknik <1254583588@qq.com>
2024-12-06 16:35:58 +0800

    找回密码

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,5 @@
 // index.js
-const backgroundUrl = 'https://pic.imgdb.cn/item/6725bebdd29ded1a8c600e6d.jpg'
-const newUrl ='https://pic.imgdb.cn/item/6741f55bd29ded1a8c345248.jpg'
+const backgroundUrl ='https://pic.imgdb.cn/item/6741f55bd29ded1a8c345248.jpg'
 const nameUrl = 'https://pic.imgdb.cn/item/6721e774d29ded1a8c1f12cf.png'
 const seeUrl = 'https://pic.imgdb.cn/item/67262438d29ded1a8cafcac2.png'
 const unseeUrl = 'https://pic.imgdb.cn/item/6726243cd29ded1a8cafcdb2.png'
@@ -8,7 +7,6 @@ const unseeUrl = 'https://pic.imgdb.cn/item/6726243cd29ded1a8cafcdb2.png'
 Page({
   data: {
     backgroundUrl: backgroundUrl,
-    newUrl: newUrl,
     nameUrl: nameUrl,
     activeTab: 'password',
     userPhone: '',
@@ -36,6 +34,12 @@ Page({
 
   },
 
+  onForgotPassword(){
+    wx.navigateTo({
+        url: '/pages/login/retrieve/retrieve'
+      });
+  },
+
   onLogin(){
     wx.navigateTo({
         url: '/pages/dashboard/dashboard'
@@ -44,7 +48,7 @@ Page({
 
   onRegister(){
     wx.navigateTo({
-      url: '/pages/register/register'
+      url: '/pages/login/register/register'
     });
   }
 })
