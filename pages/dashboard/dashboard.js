9d54c97fce5f5137e89dadeb1e115e9d8f77f686
oknik <1254583588@qq.com>
2024-12-08 18:55:19 +0800

    绘本

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -17,8 +17,8 @@ const features = [
     imageUrl: "https://pic.imgdb.cn/item/67349a3cd29ded1a8c9805dd.jpg"
   },
   {
-    path: "pictureBook",
-    name: "绘本",
+    path: "read/select/select",
+    name: "悦读绘本园",
     imageUrl: "https://pic.imgdb.cn/item/6734a0ccd29ded1a8c9d2211.jpg"
   },
   {
